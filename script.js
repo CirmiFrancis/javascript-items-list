7db5bc1093f435e98d27d06447ccccb707ecf3c5
Francis Cirmi <cirmifrancis@gmail.com>
2024-03-11 23:51:49 -0300

    Responsive mejorado, sweetalert2 agregado.

--- script.js
+++ script.js
@@ -16,23 +16,23 @@ function lengthItems() {
 // ADD ITEM
 btnAdd.addEventListener('click', () => {
     if (inputTitle.value !== '' && inputText.value !== '') {
-        addItem(inputTitle.value, inputText.value);
+        addItem(inputTitle.value, inputText.value, true);
         saveLocalStorage();
     } 
     else {
-        alert('Completa ambos campos.');
+        sweetAlert("Completa ambos campos.", "info");
     }
 });
 
-function addItem(title, text) {
+function addItem(title, text, boolean) {
     const divItem = document.createElement('div');
     divItem.innerHTML = 
     `
         <li class="item container-fluid border border-dark rounded mb-2">
-            <div class="row d-flex justify-content-center align-items-center">
+            <div class="row d-flex justify-content-center align-items-center py-2">
                 <div class="col-10 col-md-11 text-start border-end border-dark">
-                    <h3 class="title overflow-auto text-uppercase pt-3 m-0">${title}</h3>
-                    <p class="overflow-auto pb-3 m-0">${text}</p>
+                    <h3 class="title overflow-auto text-uppercase m-0">${title}</h3>
+                    <p class="overflow-auto m-0">${text}</p>
                 </div>
                 <div class="col-2 col-md-1 d-flex justify-content-center">
                     <button class="btnDelete border-0 rounded text-danger bg-white fs-1">X</button>
@@ -40,12 +40,12 @@ function addItem(title, text) {
             </div>
         </li>
     `;
-
     itemsList.appendChild(divItem);
     inputTitle.value = '';
     inputText.value = '';
     searchItem.value = '';
     lengthItems();
+    boolean ? sweetAlert("Item agregado.", "success") : '';
 };
 
 // REMOVE ITEM
@@ -54,6 +54,7 @@ itemsList.addEventListener('click', (e) => {
         e.target.closest('.item').parentElement.remove();
         saveLocalStorage();
         lengthItems();
+        sweetAlert("Item eliminado.", "error");
     }
 });
 
@@ -72,7 +73,6 @@ function search(searchString) {
     });
 };
 
-
 // LOCAL STORAGE
 function saveLocalStorage() {
     const items = Array.from(itemsList.children).map(item => ({
@@ -90,7 +90,20 @@ function loadLocalStorage() {
         const parsedItems = JSON.parse(storedItems);
 
         parsedItems.forEach(item => {
-            addItem(item.title, item.text);
+            addItem(item.title, item.text, false);
         });
     }
 };
+
+// SWEET ALERT 2
+function sweetAlert(title, icon) {
+    Swal.fire({
+        toast: true,
+        position: "bottom-end",
+        timer: 2000,
+        title: title,
+        icon: icon,
+        showConfirmButton: false,
+        timerProgressBar: true
+    });
+}
